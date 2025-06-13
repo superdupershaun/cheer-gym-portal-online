d3f7568426156c0ffdb4af4fb4733b85dd143f4e
superdupershaun <superdupershaun@yahoo.com>
2025-06-13 02:18:06 +0000

    Set correct Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures relative paths for GitHub Pages
+  base: '/cheer-gym-portal-online/',
   build: {
     outDir: 'dist',
   },
